fc3304000bc2db5deb8fcf386938d7186bad8a9d
Joaquin Piqueras Delicado <Joakanpde@gmail.com>
2025-04-28 01:05:00 +0200

    nuevo estilo para el login y responsive

--- src/api/config.js
+++ src/api/config.js
@@ -1,12 +1,12 @@
 // Voy a crear un archivo de configuración para la API
 // Si option es "local", la API será localhost/api y si es "backend", la API será localhost/backend/api
-const option = "backend";
+const option = "local";
 
 let API_BASE; // Declare API_BASE outside the blocks
 
 if (option === "local") {
     // Localhost
-    API_BASE = 'http://localhost/EducaCenter_Backend/api';
+    API_BASE = 'http://localhost/EducaCenter/api';
 }
 if (option === "backend") { 
     // Backend
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
-import { Box, Button, TextField, Typography, Paper, CircularProgress } from '@mui/material';
+import { Box, Button, TextField, Typography, Paper, CircularProgress, InputAdornment, IconButton } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { motion } from 'framer-motion';
 import { loginUser } from '../api/login';
+import backgroundImage from '../assets/background_login.png';
+import logo from '../assets/logo.png';
 
-// Este componente se encargará de gestionar el inicio de sesión de los usuarios.
-// Si el usuario inicia sesión correctamente, se guardará el token en sessionStorage y se redirigirá a la página principal de la aplicación.
 export default function Login({ onLogin }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,63 +39,131 @@ export default function Login({ onLogin }) {
 
   return (
     <Box
-      component={motion.div}
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
       sx={{
-        height: '100vh', 
-        backgroundColor: '#f5f5f5',
+        height: '100vh',
+        width: '100vw',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        p: 2,
-        boxSizing: 'border-box', 
-        overflow: 'hidden' 
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden'
       }}
     >
-      <Paper elevation={6} sx={{ p: 4, width: '100%', maxWidth: 400 }}>        
-        <Typography variant="h5" fontWeight="bold" mb={3} textAlign="center">
-          Inicio de Sesión
-        </Typography>
+      {/* Sección izquierda con la imagen */}
+      <Box
+        component={motion.div}
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+        sx={{
+          flex: { xs: '0', md: '1' }, // En móviles oculta la imagen, en pc ocupa 50%
+          display: { xs: 'none', md: 'block' }, // Oculta en móviles
+          overflow: 'hidden',
+          height: '100%',
+        }}
+      >
+        <img
+          src={backgroundImage}
+          alt="EducaCenter"
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      </Box>
 
-        <form onSubmit={handleSubmit}>
-          <TextField
-            fullWidth
-            label="Email"
-            variant="outlined"
-            margin="normal"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            label="Contraseña"
-            type="password"
-            variant="outlined"
-            margin="normal"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+      {/* Sección derecha con el login */}
+      <Box
+        component={motion.div}
+        initial={{ opacity: 0, x: 20 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+        sx={{
+          flex: '1',
+          flexDirection: 'column',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#e3f2fd',
+          p: 3,
+        }}
+      >
+        <Box display="flex" justifyContent="center" mb={3}>
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: 'auto', height: 100 }}
+            />
+        </Box>
+        <Paper
+          elevation={6}
+          component={motion.div}         
+          sx={{
+            p: 4,
+            width: '100%',
+            maxWidth: 400,
+            backgroundColor: 'rgba(255, 255, 255, 0.9)',
+            transition: 'opacity 0.3s ease-in-out',
+          }}
+        >          
+          <Typography variant="h5" fontWeight="bold" mb={3} textAlign="center">
+            Inicio de Sesión
+          </Typography>
 
-          {error && (
-            <Typography color="error" mt={1}>
-              {error}
-            </Typography>
-          )}
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              label="Contraseña"
+              type={showPassword ? 'text' : 'password'}
+              variant="outlined"
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 3 }}
-            disabled={loading}
-          >
-            {loading ? <CircularProgress size={24} color="inherit" /> : 'Entrar'}
-          </Button>
-        </form>
-      </Paper>
+            {error && (
+              <Typography color="error" mt={1}>
+                {error}
+              </Typography>
+            )}
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{ mt: 3 }}
+              disabled={loading}
+            >
+              {loading ? <CircularProgress size={24} color="inherit" /> : 'Entrar'}
+            </Button>
+          </form>
+        </Paper>
+      </Box>
     </Box>
   );
 }
