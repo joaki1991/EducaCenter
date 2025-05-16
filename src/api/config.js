8eadabc85ec2d2d7009e43c571addec251cf0f5b
Joaquin Piqueras Delicado <Joakanpde@gmail.com>
2025-05-16 18:32:04 +0200

    correccion backend

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,10 @@
 // Voy a crear un archivo de configuración para la API
 // Si option es "local", la API será localhost/api y si es "backend", la API será localhost/backend/api
-const option = "local";
+const option = "backend";
 
 let API_BASE; // Declare API_BASE outside the blocks
 
-if (option === "backend") {
+if (option === "local") {
     // Localhost
     API_BASE = 'http://localhost/EducaCenter/api';
 }
